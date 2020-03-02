37755dd3788ab467efdb1d3156d7d11b7d43558e
mistermoustache <anto@mistermoustache.ch>
2020-03-02 16:06:25 +0100

    main nav
    adding background gradient and nav feature

--- assets/js/website.js
+++ assets/js/website.js
@@ -1,4 +1,8 @@
 $(function(){
 
 });
-
+document.getElementById("hamburger-button").addEventListener("click",function(){
+    document.getElementById("hamburger-button").classList.toggle("clicked");  
+    document.querySelector("nav").classList.toggle("clicked"); 
+});
+  
