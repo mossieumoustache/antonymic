19e360585bb6a73706071369901d96a577a849d8
mister moustache <anthony@925.ch>
2020-03-23 22:29:13 +0100

    MULTILANG second commit
    add translation for common text and for content of homepage/team

--- assets/js/website.js
+++ assets/js/website.js
@@ -1,4 +1,22 @@
-document.getElementById("hamburger-button").addEventListener("click",function(){
-    document.getElementById("hamburger-button").classList.toggle("clicked");  
-    document.querySelector("nav").classList.toggle("clicked"); 
+import Translator from "./antonymicTranslator.js";
+
+document.querySelector("form").addEventListener("click", function(evt) {
+    if (evt.target.tagName === "INPUT") {
+        translator.load(evt.target.value);
+    }
 });
+
+document.getElementById("hamburger-button").addEventListener("click",function(){
+    document.getElementById("hamburger-button").classList.toggle("clicked");
+    document.querySelector("nav").classList.toggle("clicked");
+});
+
+var translator = new Translator({
+    persist: false,
+    languages: ["en", "fr"],
+    defaultLanguage: "en",
+    detectLanguage: true,
+    filesLocation: "/assets/languages"
+});
+
+translator.load();
